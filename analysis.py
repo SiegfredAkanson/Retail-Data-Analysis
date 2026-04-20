3f8cef7ffe87e3bf092f3ecf66b8ba70e2cba3a8
Siegfred <alexisakanson@gmail.comSu04nny08>
2026-04-20 10:08:52 -0700

    feat: creating dataframe and calculating monthly sales

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pandas._config import dates
 
 #Part 2 : Numpy
 np.random.seed(42)
@@ -11,3 +12,13 @@ std = np.std(sales)
 surge_days = sales > (mean+1.5*std)
 labels = np.where(surge_days, "High", "Normal")
 print(labels[:10])
+
+#Part 2: Pandas
+dates = pd.date_range(start="2025-01-01", periods=365) #creating dataframes
+df = pd.DataFrame({"Date":dates, "Sales":sales})
+
+df["Rev_after_tax"] = df["Sales"]*0.85 #Revenue after tax
+df["Month"] = df["Date"].dt.month
+monthly_sales = df.groupby("Month")["Sales"].sum() #Monthly Total Sales
+print(monthly_sales)
+
